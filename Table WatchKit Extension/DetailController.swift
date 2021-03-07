7425473d54a5cf132460978864a2a76a5f6a5d6d
Sergey Lavrov <lavrov-sergey@yandex.ru>
2021-03-08 01:07:37 +0300

    Create table and open detail

--- Table WatchKit Extension/DetailController.swift
+++ Table WatchKit Extension/DetailController.swift
@@ -1,5 +1,5 @@
 //
-//  InterfaceController.swift
+//  DetailController.swift
 //  Table WatchKit Extension
 //
 //  Created by Сергей Лавров on 08.03.2021.
@@ -9,21 +9,24 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+class DetailController: WKInterfaceController {
 
-    @IBOutlet weak var table: WKInterfaceTable!
+    @IBOutlet weak var titleLabel: WKInterfaceLabel!
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        table.setNumberOfRows(Model.getData().count, withRowType: "tableId")
+        
+        // Configure interface objects here.
     }
-    
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
     }
-    
+
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
     }
 
 }
---
+++ Table WatchKit Extension/TableRowController.swift
@@ -0,0 +1,16 @@
+//
+//  TableRowController.swift
+//  Table WatchKit Extension
+//
+//  Created by Сергей Лавров on 08.03.2021.
+//
+
+import WatchKit
+import Foundation
+
+
+class TableRowController: NSObject {
+
+    @IBOutlet weak var title: WKInterfaceLabel!
+    
+}
